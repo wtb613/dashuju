6a19231d98d65a5bec5e53ef1c46143f8fe49e2b
wtb613 <63451272+wtb613@users.noreply.github.com>
2020-07-19 19:55:07 +0800

    Update ExitServlet.java

--- src/main/java/com/offcn/servlet/ExitServlet.java
+++ src/main/java/com/offcn/servlet/ExitServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-
+// 不用加@Controller @ServletComponentScan 会自动扫描 
 @WebServlet("/ExitServlet")
 public class ExitServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
